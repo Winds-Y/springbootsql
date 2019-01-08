d501929d69d635ddda95c2fb3a017e494a6fe653
ycz <2155513297@qq.com>
2019-01-08 11:09:13 +0800

    use jparepository

--- src/main/java/com/example/springbootsql/repository/UserRepository.java
+++ src/main/java/com/example/springbootsql/repository/UserRepository.java
@@ -1,8 +1,8 @@
 package com.example.springbootsql.repository;
 
 import com.example.springbootsql.entity.User;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
