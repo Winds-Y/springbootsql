d5d96b8359b3068638741a388a481112580e893e
harb <7505317857@qq.com>
2019-03-30 19:31:11 +0800

    add content 330

--- src/main/java/com/example/springbootsql/controller/BodyController.java
+++ src/main/java/com/example/springbootsql/controller/BodyController.java
@@ -1,8 +1,10 @@
 package com.example.springbootsql.controller;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import com.example.springbootsql.entity.User;
 import com.example.springbootsql.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,15 +20,18 @@ public class BodyController {
         this.userRepository = userRepository;
     }
 
+
     @GetMapping("/greeting")
     public String greetingForm(Model model) {
         model.addAttribute("user", new User());
+        System.out.println("in greetingForm");
         return "greeting";
     }
 
     @PostMapping("/greeting")
     public String greetingSubmit(@ModelAttribute User user) {
-
+        System.out.println("in greetingSubmit");
+        System.out.println(user.getAge());
         User newUser = new User();
 
         newUser.setName(user.getName());
@@ -42,10 +47,37 @@ public class BodyController {
 
     @GetMapping("/all")
     public String getMessage(Model model) {
-
         Iterable<User> users = userRepository.findAll();
-
         model.addAttribute("users", users);
         return "all";
+    }
+
+    @GetMapping("/login")
+    public String loginPage(Model model){
+        model.addAttribute("user", new User());
+        System.out.println("in loginPage");
+        return "login";
+    }
+    @GetMapping("/tt")
+    public String getStatic(){
+        return "redirect:/success.html";
+    }
+
+    @PostMapping("/loginResult")
+    public String loginResult(@ModelAttribute User user){
+        System.out.println(user.getName());
+        String name=user.getName();
+        User newUser;
+        newUser=userRepository.findByName(name);
+        if(newUser==null){
+            System.out.println("in loginResult:用户不存在");
+            return "redirect:/loginErr.html";
+        }else {
+            System.out.println("in loginResult:登陆成功");
+            System.out.println(newUser.getCity());
+            System.out.println(newUser.getEmail());
+            System.out.println("in loginResult");
+            return "redirect:/index.html";
+        }
     }
 }
